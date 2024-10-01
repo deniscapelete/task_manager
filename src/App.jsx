35d34602d2b01dc4b2d973a700821f48a42cdb39
Denis <capelete.denis@hotmail.com>
2024-10-01 20:07:40 -0300

    criando o componente task

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,55 @@ import { Tasks } from "./components/Tasks"
 import { AddTasks } from "./components/AddTask"
 
 export function App() {
-  //State (Estado)
-  const [message, setMessage] = useState('Olá Mundo')
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Estudar programação',
+      description: 'Estudar para se tornar um desenvolvedor full stack.',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: 'Caminhada',
+      description: 'Caminhada para melhora da saúde.',
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map(task => {
+      // Preciso atualizar essa tarefa.
+      if (task.id == taskId) {
+        return { ...task, isCompleted: !task.isCompleted }
+      }
+
+      // Não preciso atualizar essa tarefa.
+      return task
+    });
+
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter(task => task.id != taskId);
+
+    setTasks(newTasks);
+  }
 
   return (
-    <div>
-      <h1>Gerenciador de tarefas</h1>
-      <AddTasks />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <AddTasks />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
     </div>
   )
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,28 @@
-export function Tasks() {
+import { ChevronRightIcon, Trash } from "lucide-react"
+import { useState } from "react"
+
+export function Tasks({
+    tasks,
+    onTaskClick,
+    onDeleteTaskClick,
+}) {
+
     return (
-        <h1>Tasks</h1>
+        <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+            {tasks
+                .map(task =>
+                    <li key={task.id} className="flex gap-2">
+                        <button onClick={() => onTaskClick(task.id)} className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && 'line-through'}`}>
+                            {task.title}
+                        </button>
+                        <button className="bg-slate-400 text-white p-2 rounded-md">
+                            <ChevronRightIcon />
+                        </button>
+                        <button onClick={() => onDeleteTaskClick(task.id)} className="bg-slate-400 text-white p-2 rounded-md">
+                            <Trash />
+                        </button>
+                    </li>
+                )}
+        </ul>
     )
 }
