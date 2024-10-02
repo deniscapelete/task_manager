b87b70060f5463cbca4e8558796471d4909e29a3
Denis <capelete.denis@hotmail.com>
2024-10-02 20:08:48 -0300

    criando o componente de add task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Tasks } from "./components/Tasks"
 import { AddTasks } from "./components/AddTask"
+import { v4 } from "uuid"
 
 export function App() {
 
@@ -39,13 +40,25 @@ export function App() {
     setTasks(newTasks);
   }
 
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title, // como o nome da propriedade é o mesmo do parametro nao precisa fazer "title:title".
+      description,
+      isCompleted1: false,
+    }
+    setTasks([...tasks, newTask]) // quando atualiza um estado e ele é uma lista não pode usar ".push", não funciona.
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de tarefas
         </h1>
-        <AddTasks />
+        <AddTasks
+          onAddTaskSubmit={onAddTaskSubmit}
+        />
         <Tasks
           tasks={tasks}
           onTaskClick={onTaskClick}
